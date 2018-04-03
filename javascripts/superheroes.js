64a213dec19d9b920d7004b8d142fa676144bfac
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-02 21:33:39 -0500

    added xhr for superheroes page

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -1,1 +1,38 @@
-console.log("superheroes");
+// console.log("superheroes");
+
+const printToDom = (domString, divId) => {
+  document.getElementById(divId).innerHTML = domString;
+};
+
+const buildDomString = (array) => {
+  let domString = "";
+  array.forEach((card)=>{
+    domString += `<div class="card">`;
+    domString +=    `<h2>${card.name}</h2>`;
+    domString += `<div class="card">`;
+  })
+ printToDom(domString, "card-holder");
+console.log(domString);
+};
+
+
+function executeThisCodeIfXHRFails () {
+
+
+}
+
+function executeThisCodeAfterFileLoaded () {
+  const data = JSON.parse(this.responseText);
+  buildDomString(data.superheroes);
+  console.log(data.superheroes);
+}
+
+const startApplication = () => {
+  let myRequest = new XMLHttpRequest();
+  myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+  myRequest.addEventListener("error", executeThisCodeIfXHRFails);
+  myRequest.open("GET","../db/superheroes.json");
+  myRequest.send();
+};
+
+startApplication ();
