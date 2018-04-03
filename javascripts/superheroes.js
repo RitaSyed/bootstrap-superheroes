e91779a39caf16d3fab30e74282f544284b7cffa
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-03 18:33:28 -0500

    added styles to superheroes page

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -6,13 +6,25 @@ const printToDom = (domString, divId) => {
 
 const buildDomString = (array) => {
   let domString = "";
-  array.forEach((card)=>{
-    domString += `<div class="card">`;
-    domString +=    `<h2>${card.name}</h2>`;
-    domString += `<div class="card">`;
+  array.forEach((heroes)=>{
+    // domString += `<div class="card">`;
+    // domString +=    `<h2>${card.name}</h2>`;
+    // domString += `<div class="card">`;
+     domString += `<div class="col-sm-3">`;
+     domString += `<div class="panel">`;
+     domString +=   `<div class="panel-heading">`;
+     domString +=      `<h3 class="panel-title">${heroes.name}</h3>`;
+     domString +=   `</div>`;
+     domString +=    `<div class="panel-body">`;
+     domString +=       `<img class="charImage" src="${heroes.image}">`;
+     domString +=       `<p class="charDescription">${heroes.description}"<p>`;
+     domString +=    `</div>`;
+     domString += `</div>`;
+     domString += `</div>`;
+    // domString += `<div class="card">`;
   })
  printToDom(domString, "card-holder");
-console.log(domString);
+// console.log(domString);
 };
 
 
@@ -31,7 +43,7 @@ const startApplication = () => {
   let myRequest = new XMLHttpRequest();
   myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
   myRequest.addEventListener("error", executeThisCodeIfXHRFails);
-  myRequest.open("GET","../db/superheroes.json");
+  myRequest.open("GET", "../db/superheroes.json");
   myRequest.send();
 };
 
